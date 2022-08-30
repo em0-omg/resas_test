54ea04855c25fc3fb4830d0c479b691c11f23811
em0-omg <em0818.omg@gmail.com>
2022-08-31 00:32:04 +0900

    注意書きをリスト形式に修正

--- src/features/prefectures/components/PrefectureCheckboxNote/PrefectureCheckboxNote.tsx
+++ src/features/prefectures/components/PrefectureCheckboxNote/PrefectureCheckboxNote.tsx
@@ -4,10 +4,15 @@ const PrefectureCheckboxNote = () => {
   return (
     <div>
       <h3 className={styles.guide}>表示する都道府県を選択してください</h3>
-      <p className={styles.note}>
-        ※ チェックがない場合はサンプルとして北海道のデータを表示します。
-      </p>
-      <p className={styles.note}>※ 複数選択した場合は合算で表示されます。</p>
+      <ul className={styles.list}>
+        <li className={styles.note}>
+          ※ チェックがない場合はサンプルとして北海道のデータを表示します。
+        </li>
+        <li className={styles.note}>
+          ※
+          複数選択した場合は合算で表示されます。一度に選択できるのは10個の都道府県までです。
+        </li>
+      </ul>
     </div>
   );
 };
