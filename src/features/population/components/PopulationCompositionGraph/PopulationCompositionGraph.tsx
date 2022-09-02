8f458c583ed548fc665768c1b4a942c21846930c
em0-omg <em0818.omg@gmail.com>
2022-09-02 19:28:23 +0900

    人口構成グラフコンポーネントのテストを実装

--- src/features/population/components/PopulationCompositionGraph/PopulationCompositionGraph.tsx
+++ src/features/population/components/PopulationCompositionGraph/PopulationCompositionGraph.tsx
@@ -1,22 +1,10 @@
-import {
-  CartesianGrid,
-  Legend,
-  Label,
-  Line,
-  LineChart,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
-
 import useQueryPopulation from '@/hooks/useQueryPopulation';
-import useResponsive from '@/hooks/useResponsive';
 import Spinner from '@/components/Elements/Spinner/Spinner';
 
 import styles from './PopulationCompositionGraph.module.scss';
+import LineChartView from '../LineChartView/LineChartView';
 
 const PopulationCompositionGraph = () => {
-  const { isDesktop } = useResponsive();
   const { status, data } = useQueryPopulation();
 
   if (status === 'loading')
@@ -37,28 +25,7 @@ const PopulationCompositionGraph = () => {
       {data.data.map((d) => (
         <div key={d.label} className={styles.graph}>
           <h2 className={styles.title}>{d.label}</h2>
-          <LineChart
-            width={isDesktop ? 400 : 320}
-            height={isDesktop ? 300 : 240}
-            data={d.data}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="year">
-              <Label value="年" offset={0} position="bottom" />
-            </XAxis>
-            <YAxis
-              label={{ value: '人口数', angle: -90, position: 'insideLeft' }}
-            />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="value"
-              stroke="#82ca9d"
-              aria-label="人口数"
-            />
-          </LineChart>
+          <LineChartView data={d} />
         </div>
       ))}
     </div>
