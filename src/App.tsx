7c65f95f09c22fa21b5a0a5b9a172f625091fb37
em0-omg <em0818.omg@gmail.com>
2022-09-02 18:11:39 +0900

    エラーメッセージのテストを実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AppProvider } from '@/providers/app';
 import Layout from '@/components/Layout/Layout';
 import PrefectureCheckboxes from '@/features/prefectures/components/PrefectureCheckboxes/PrefectureCheckboxes';
 import PopulationCompositionGraph from '@/features/population/components/PopulationCompositionGraph/PopulationCompositionGraph';
+import ErrorMessage from '@/features/messages/components/ErrorMessage';
 
 import './App.scss';
 
@@ -10,6 +11,7 @@ const App = () => {
     <AppProvider>
       <Layout>
         <PrefectureCheckboxes />
+        <ErrorMessage />
         <PopulationCompositionGraph />
       </Layout>
     </AppProvider>
